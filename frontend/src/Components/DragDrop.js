4cf974949adeb68212340812cbb5f55304179820
sarjil patel <sarjilpatel2903@gmail.com>
2022-10-31 18:31:53 +0530

    changes

--- frontend/src/Components/DragDrop.js
+++ frontend/src/Components/DragDrop.js
@@ -9,94 +9,94 @@ const charactersList = [
   {
     _id: "a",
     character: "a",
+    isOperator: false,
     url: "https://res.cloudinary.com/djnkkdrhb/image/upload/v1667211270/dnd/A_hdongs.png",
   },
   {
     _id: "b",
     character: "b",
+    isOperator: false,
     url: "https://res.cloudinary.com/djnkkdrhb/image/upload/v1667211269/dnd/B_cgxh91.png",
   },
   {
     _id: "c",
     character: "c",
+    isOperator: false,
     url: "https://res.cloudinary.com/djnkkdrhb/image/upload/v1667211270/dnd/C_przgq6.png",
   },
   {
     _id: "d",
     character: "d",
+    isOperator: false,
     url: "https://res.cloudinary.com/djnkkdrhb/image/upload/v1667211269/dnd/D_tjfkut.png",
   },
   {
     _id: "e",
     character: "e",
+    isOperator: false,
     url: "https://res.cloudinary.com/djnkkdrhb/image/upload/v1667211269/dnd/E_c0geqo.png",
   },
   {
     _id: "f",
     character: "f",
+    isOperator: false,
     url: "https://res.cloudinary.com/djnkkdrhb/image/upload/v1667211269/dnd/F_uztzvp.png",
   },
   {
     _id: "g",
     character: "g",
+    isOperator: false,
     url: "https://res.cloudinary.com/djnkkdrhb/image/upload/v1667211269/dnd/G_rzgjtb.png",
   },
-];
-
-const operatorsList = [
   {
-    _id: "635f816b65280ed7141c33be",
+    _id: "plus",
     character: "+",
-    value: "+",
     isOperator: true,
-    __v: 0,
+    url: "https://res.cloudinary.com/djnkkdrhb/image/upload/v1667217317/dnd/qukjcv55vw4ynj13e7sh.png",
   },
   {
-    _id: "635f817465280ed7141c33c1",
+    _id: "minus",
     character: "-",
-    value: "-",
     isOperator: true,
-    __v: 0,
+    url: "https://res.cloudinary.com/djnkkdrhb/image/upload/v1667217317/dnd/div_n616d2.png",
   },
   {
     _id: "635f817b65280ed7141c33c4",
     character: "/",
-    value: "/",
     isOperator: true,
-    __v: 0,
+    url: "https://res.cloudinary.com/djnkkdrhb/image/upload/v1667217317/dnd/qukjcv55vw4ynj13e7sh.png",
   },
   {
     _id: "635f818365280ed7141c33c7",
     character: "*",
-    value: "*",
     isOperator: true,
-    __v: 0,
+    url: "https://res.cloudinary.com/djnkkdrhb/image/upload/v1667217317/dnd/multiply_fwxom2.png",
   },
   {
     _id: "635f818965280ed7141c33ca",
     character: "=",
-    value: "=",
     isOperator: true,
-    __v: 0,
+    url: "https://res.cloudinary.com/djnkkdrhb/image/upload/v1667217317/dnd/eq_mnbitx.png",
   },
   {
     _id: "635f818f65280ed7141c33cd",
     character: ">",
-    value: ">",
     isOperator: true,
-    __v: 0,
+    url: "https://res.cloudinary.com/djnkkdrhb/image/upload/v1667217317/dnd/gt_rpmymh.png",
   },
   {
     _id: "635f819765280ed7141c33d0",
     character: "<",
-    value: "<",
+
     isOperator: true,
-    __v: 0,
+    url: "https://res.cloudinary.com/djnkkdrhb/image/upload/v1667217317/dnd/lt_ytcgsu.png",
   },
 ];
 
 const DragDrop = () => {
   const [board, setBoard] = useState([]);
+  const [charFlag, setCharFlag] = useState(true);
+  const [oprFlag, setOprFlag] = useState(false);
 
   const [{ isOver }, drop] = useDrop(() => ({
     accept: "div",
@@ -107,23 +107,46 @@ const DragDrop = () => {
   }));
 
   const addImageToBoard = (id) => {
-    const characterList = charactersList.filter((picture) => id === picture.id);
-    setBoard((board) => [...board, characterList[0]]);
+    // const characterList = charactersList.filter(
+    //   (picture) => id === picture._id
+    // );
+    const flag = oprFlag;
+
+    if (id.isOperator === oprFlag) {
+      setOprFlag(!flag);
+      setBoard((board) => [...board, id]);
+      console.log(id.isOperator);
+      console.log(oprFlag);
+      console.log(board);
+    } else {
+      console.log(board);
+    }
   };
 
   return (
-    <>
+    <div className="wrapper">
       <div className="characters">
         {charactersList.map((character) => {
-          return <Element url={character.url} id={character.id} />;
+          if (!character.isOperator) {
+            return <Element url={character.url} id={character} />;
+          }
         })}
       </div>
+
+      <div className="characters">
+        {charactersList.map((character) => {
+          if (character.isOperator) {
+            return <Element url={character.url} id={character} />;
+          }
+        })}
+      </div>
+
       <div className="Board" ref={drop}>
         {board.map((picture) => {
-          return <Element url={picture.url} id={picture.id} />;
+          return <Element url={picture.url} id={picture} />;
         })}
       </div>
-    </>
+    </div>
   );
 };
 

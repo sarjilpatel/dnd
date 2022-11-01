551333973f03f8b9275ca2bdd71a2a531de191aa
sarjil patel <sarjilpatel2903@gmail.com>
2022-11-01 08:23:08 +0530

    minor changes

--- backend/configs/database.js
+++ backend/configs/database.js
@@ -1,10 +1,10 @@
 const mongoose = require("mongoose");
-const MONGO_URL =
-  "mongodb+srv://sarjilpatel:SARjil543@cluster0.ebwhdhf.mongodb.net/?retryWrites=true&w=majority";
+MONGO_URL =
+  "mongodb+srv://sarjilpatel:SARjil543@cluster0.x0ehsvt.mongodb.net/?retryWrites=true&w=majority";
 
 exports.connectDatabase = () => {
   mongoose
-    .connect(process.env.MONGO_URL)
+    .connect(MONGO_URL)
     .then((con) => console.log(`database connected : ${con.connection.host}`))
     .catch((err) => {
       console.log(err.message);
